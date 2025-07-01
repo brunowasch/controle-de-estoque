838086d7438718d0882f620ff38e87c6aa6c7264
ValGaspar <valentina113457@gmail.com>
2025-07-01 02:30:53 -0300

    Cadastro ok! (Ainda em desenvolvimento)

--- src/app.js
+++ src/app.js
@@ -1,17 +1,22 @@
 require('dotenv').config(); // Carrega variáveis do .env
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./database');
 
 const app = express();
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(express.json());
 app.use(cors());
 
+
 app.get('/', (req, res) => {
-    res.send("Bem-vindo ao sistema de estoque do Só Bujiganga!")
-})
+    res.render('cadastro');
+});
 connectDB().then(() => {
     app.listen(process.env.PORT, () => {
     console.log(`Servidor rodando na http://localhost:${process.env.PORT}`);
     });
-})
+});
