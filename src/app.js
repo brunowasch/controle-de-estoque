b8ecf95a19fa0a9a55504f0e88d0d49db62b694e
André <=>
2025-07-01 13:19:24 -0300

    Resolvendo conflitos do merge

--- src/app.js
+++ src/app.js
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
 require('dotenv').config(); 
 const authRoutes = require('./routes/authRoutes');
 const authMiddleware = require('./middlewares/authMiddleware');
-=======
-require('dotenv').config(); // Carrega variáveis do .env
 const path = require('path');
->>>>>>> develop
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./database');
@@ -17,24 +13,17 @@ app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(express.json());
 app.use(cors());
 
-<<<<<<< HEAD
 app.use('/api/auth', authRoutes);
 
 connectDB().then(() => {
     app.listen(process.env.PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${process.env.PORT}`);
-=======
+        console.log(`Servidor rodando em http://localhost:${process.env.PORT}`);
+    });
+});
 
 app.get('/', (req, res) => {
     res.render('cadastro');
 });
 app.get('/login', (req, res) => {
-  res.render('login');
-});
-
-connectDB().then(() => {
-    app.listen(process.env.PORT, () => {
-    console.log(`Servidor rodando na http://localhost:${process.env.PORT}`);
->>>>>>> develop
-    });
+    res.render('login');
 });
