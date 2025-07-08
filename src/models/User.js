e1eee3bcb0281e12729861400a6f2bbbc7a6ac25
ValGaspar <valentina113457@gmail.com>
2025-07-07 22:40:24 -0300

    Login e Cadastro funcionando!

--- src/models/User.js
+++ src/models/User.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    email: {type: String, required: true, unique: true},
-    password: {type: String, required: true, unique: true},
+    nome: { type: String, required: false },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true, unique: true },
     resetPasswordToken: String,
     resetPasswordExpires: Date,
     refreshToken: String
